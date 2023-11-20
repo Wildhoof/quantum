3d9e62dcf838ce50197960c5f802a59ed1f680e8
Wildhoof <kontakt@equidea-world.de>
2023-11-20 01:14:52 +0100

    Base files
    Adds first files to the repo

---
+++ src/Quantum/Quantum.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Quantum;
+
+/**
+ * Quantum framework.
+ */
+class Quantum
+{
+    /**
+     * The current version of the Quantum framework package.
+     */
+    const VERSION = '0.1.0';
+
+    const MAJOR_VERSION = 0;
+    const MINOR_VERSION = 1;
+    const RELEASE_VERSION = 0;
+}
