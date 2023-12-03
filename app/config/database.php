67b553a8cbc98987958d8c56ab6a99b5acf99a3f
Wildhoof <kontakt@equidea-world.de>
2023-12-03 21:19:44 +0100

    Add Database service and config

---
+++ app/config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'hostname' => 'localhost',
+    'database' => 'quantum',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4'
+];
--- app/config/services.php
+++ app/config/services.php
@@ -1,7 +1,29 @@
 <?php
 
+use Quantum\Database\Database;
+use Quantum\Database\Query;
+
 use Quantum\View;
 
 $app->container()->append(View::class, new View(
     __DIR__ . '/../views/'
 ));
+
+// Uncomment, if you want to use the database in your application
+
+/*
+$config = include 'database.php';
+
+$app->container()->append(Database::class, new Database(
+    hostname: $config['hostname'],
+    database: $config['database'],
+    charset: $config['charset'],
+    username: $config['username'],
+    password: $config['password']
+));
+
+$app->container()->append(
+    Query::class,
+    $app->container()->get(Query::class)
+);
+*/
